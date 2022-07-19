61574aec19f989d01dbd48d0201eec6e80043b12
Angela Vegega <angela.vegega@gmail.com>
2022-07-19 21:23:25 +0900

    added mongo connection

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const PORT = 8000
+const MongoClient = require('mongodb').MongoClient
+const connectionString = 'mongodb+srv://karina:dwkafP465ALCqkUU@cluster0.pyftha1.mongodb.net/?retryWrites=true&w=majority'
 
 app.use(express.static('public'))
 app.use(cors())
+app.use(express.json())
 
 const foodStyles = {
   'pizza': [
     {
       'name': 'ラ・ゴロセッタ',
-      'area': 'minami-senba',
+      'area': 'Minami-Senba',
       'firstTime': false
     },
     {
@@ -22,7 +25,7 @@ const foodStyles = {
   'sushi': [
     {
       'name': 'スシロー',
-      'area': ['shinsaibashi', 'taisho', 'others'],
+      'area': ['Shinsaibashi', 'Taisho', 'others'],
       'firstTime': false
     }
   ],
@@ -68,24 +71,39 @@ const foodStyles = {
 }
 
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
-
-app.get('/api', (req, res) => {
-  res.json(foodStyles)
-})
-
-app.get('/api/:style', (req, res) => {
-  const foodStyle = req.params.style.toLowerCase()
-  // res.json(foodStyles)
-  if(foodStyles[foodStyle]) {
-    res.json(foodStyles[foodStyle])
-  } else {
-    res.json(foodStyles['default'])
-  }
+MongoClient.connect(connectionString)
+  .then(client => {
+    console.log('connnnecereted!')
+    const db = client.db('restaurants-api')
+    const infoCollection = db.collection('restaurant-info')
   
+    app.get('/', (req, res) => {
+      res.sendFile(__dirname + '/index.html')
+    })
+
+    // app.get('/api', (req, res) => {
+    //   res.json(foodStyles)
+    // })
+
+    app.get('/api/:style', (req, res) => {
+      const foodStyle = req.params.style.toLowerCase()
+      infoCollection.find({food:foodStyle}).toArray()
+      .then(results => {
+        console.log(results)
+        res.json(results)
+        
+      })
+      .catch(error => console.error(error))
+      
+      // res.json(foodStyles)
+      // if(foodStyles[foodStyle]) {
+      //   res.json(foodStyles[foodStyle])
+      // } else {
+      //   res.json(foodStyles['default'])
+      // }
+    })
 })
+  .catch(error => console.log(error))
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`catch server at ${PORT}!`);
