2415bdb16ae27a0e0968784403759eb8d38118aa
Angela Vegega <angela.vegega@gmail.com>
2022-07-20 15:29:46 +0900

    added better documentation to api homepage

--- server.js
+++ server.js
@@ -9,71 +9,9 @@ app.use(express.static('public'))
 app.use(cors())
 app.use(express.json())
 
-// const foodStyles = {
-//   'pizza': [
-//     {
-//       'name': 'ラ・ゴロセッタ',
-//       'area': 'Minami-Senba',
-//       'firstTime': false
-//     },
-//     {
-//       'name': 'Domino\'s',
-//       'area': 'delivery',
-//       'firstTime': false
-//     }
-//   ],
-//   'sushi': [
-//     {
-//       'name': 'スシロー',
-//       'area': ['Shinsaibashi', 'Taisho', 'others'],
-//       'firstTime': false
-//     }
-//   ],
-//   'yakiniku': [
-//     {
-//       'name': '蔓（つる）',
-//       'area': 'ライフの近く',
-//       'firstTime': false
-//     },
-//     {
-//       'name': '天乃',
-//       'area': '家の近く',
-//       'firstTime': false
-//     }
-//   ],
-//   'hamburgers': [
-//     {
-//       'name': 'McDonald\'s',
-//       'area': 'delivery',
-//       'firstTime': false
-//     },
-//     {
-//       'name': 'Burger King',
-//       'area': 'delivery',
-//       'firstTime': false
-//     },
-//     {
-//       'name': 'Mos burgers',
-//       'area': 'delivery',
-//       'firstTime': false
-//     },
-//     {
-//       'name': 'kua aina',
-//       'area': 'Umeda',
-//       'firstTime': false
-//     },
-//     {
-//       'name': 'Craft Burger',
-//       'area': 'Kitahorie',
-//       'firstTime': false
-//     }
-//   ]
-// }
-
-
 MongoClient.connect(connectionString)
   .then(client => {
-    console.log('connnnecereted!')
+    console.log('connnnected!')
     const db = client.db('restaurants-api')
     const infoCollection = db.collection('restaurant-info')
   
@@ -86,25 +24,15 @@ MongoClient.connect(connectionString)
       .then(results => {
         res.json(results)
       })
-      
     })
 
     app.get('/api/:style', (req, res) => {
       const foodStyle = req.params.style.toLowerCase()
       infoCollection.find({food:foodStyle}).toArray()
       .then(results => {
-        console.log(results)
         res.json(results)
-        
       })
       .catch(error => console.error(error))
-      
-      // res.json(foodStyles)
-      // if(foodStyles[foodStyle]) {
-      //   res.json(foodStyles[foodStyle])
-      // } else {
-      //   res.json(foodStyles['default'])
-      // }
     })
 })
   .catch(error => console.log(error))
